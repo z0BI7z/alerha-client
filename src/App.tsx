640a4585e93b8983cc489bcdfa81cd46ecad0bff
kuchi <burner.0107@gmail.com>
2020-10-03 12:23:00 -0400

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ErrorBoundary from "./components/ErrorBoundary";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { store, persistor } from "./modules/store";
 import RefreshLogin from "./components/RefreshLogin";
 import MainNavBar from "./components/MainNavBar";
-import Home from "./pages/Home";
-import Account from "./pages/Account";
-import Messages from "./pages/Messages";
+const Home = lazy(() => import("./pages/Home"));
+const Account = lazy(() => import("./pages/Account"));
+const Messages = lazy(() => import("./pages/Messages"));
 
 function App() {
   return (
     <Provider store={store}>
-      <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
-        <RefreshLogin>
-          <MainNavBar />
-          <BrowserRouter>
-            <Switch>
-              <Route path="/account" component={Account} />
-              <Route path="/message" component={Messages} />
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </BrowserRouter>
-        </RefreshLogin>
+      <PersistGate loading={<p>Refreshing...</p>} persistor={persistor}>
+        <ErrorBoundary>
+          <Suspense fallback={<div></div>}>
+            <RefreshLogin>
+              <MainNavBar />
+              <BrowserRouter>
+                <Switch>
+                  <Route path="/account" component={Account} />
+                  <Route path="/message" component={Messages} />
+                  <Route exact path="/" component={Home} />
+                </Switch>
+              </BrowserRouter>
+            </RefreshLogin>
+          </Suspense>
+        </ErrorBoundary>
       </PersistGate>
     </Provider>
   );
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { ErrorInfo } from "react";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
